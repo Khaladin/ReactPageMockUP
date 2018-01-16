3ae71887f26ae9b5334e70a252ae9a00009cb7df
Cory Hoban <hobancor@gmail.com>
2018-01-16 10:59:29 -0500

    Add API_KEY and state awareness

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-// Create a new component. This component should produce
-// some HTML
-const App = () => {
-	return <div>Hi!</div>;
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDui_a27fXFYTH0SeO1-p0WOYR8ijB-ZSI';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render () {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 // Take this component's generated HTML and put it
