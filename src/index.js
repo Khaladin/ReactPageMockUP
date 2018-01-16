c08c437475376c38f5858c131a63d7f580a09650
Cory Hoban <hobancor@gmail.com>
2018-01-16 11:33:56 -0500

    Added youtube list component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDui_a27fXFYTH0SeO1-p0WOYR8ijB-ZSI';
 
@@ -21,6 +22,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
